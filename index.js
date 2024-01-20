c5608ddebf099b67f78049bb1f43396969726c06
Eduardo Lange <eduardoandrelange@gmail.com>
2024-01-20 18:36:05 -0300

    agora foi

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+require('dotenv').config()
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const Despesa = require('./models/despesa')
 
-console.log('String de Conexão MongoDB:', process.env.MONGO_URI);
+const app = express()
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Conectado ao MongoDB');
-  })
-  .catch((err) => {
-    console.error('Erro ao conectar ao MongoDB:', err);
-  });
+// Middleware
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.json())
 
+// Conectar ao MongoDB
+mongoose
+	.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log('Conectado ao MongoDB')
+	})
+	.catch((err) => {
+		console.error('Erro ao conectar ao MongoDB:', err)
+	})
+
+
+
+// Rota para obter todas as despesas
+app.get('/api/despesas', async (request, response) => {
+	try {
+		const despesas = await Despesa.find({})
+		response.setHeader('Content-Type', 'application/json')
+		response.json(despesas)
+	} catch (error) {
+		console.error('Erro na consulta ao MongoDB:', error)
+		response.status(500).json({ error: 'Erro interno do servidor' })
+	}
+})
+
+// Rota padrão
 app.get('/', (req, res) => {
-  res.send('Bem-vindo à minha aplicação com MongoDB!');
-});
+	res.send('Bem-vindo à minha aplicação com MongoDB!')
+})
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- models/despesa.js
+++ models/despesa.js
@@ -1,11 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const despesaSchema = new mongoose.Schema({
-  dia: { type: Date, required: true },
-  valor: { type: Number, required: true },
-  observacao: { type: String },
-});
+	dia: { type: Date, required: true },
+	valor: { type: Number, required: true },
+	observacao: { type: String },
+})
+despesaSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	},
+})
 
-const Despesa = mongoose.model('Despesa', despesaSchema);
-
-module.exports = Despesa;
+module.exports = mongoose.model('Despesa', despesaSchema)
