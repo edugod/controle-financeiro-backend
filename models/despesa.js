b16ef2851af8a843911651e38a5a9364102daf4d
Eduardo Lange <eduardoandrelange@gmail.com>
2024-01-25 21:10:43 -0300

    ok

--- models/despesa.js
+++ models/despesa.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const despesaSchema = new mongoose.Schema({
-  dia: { type: Date, required: true },
+  dia: { type: String, required: true },
   valor: { type: Number, required: true },
   observacao: { type: String },
 });
@@ -9,7 +9,8 @@ const despesaSchema = new mongoose.Schema({
 // Definir o formato desejado ao serializar para JSON
 despesaSchema.set('toJSON', {
   transform: function (doc, ret) {
-    ret.dia = ret.dia.toLocaleDateString(); // Ajuste para formatar como DD/MM/YYYY
+    // tirei essa merda pq eu tive que enviar uma string invés de data.
+    // ret.dia = ret.dia.toLocaleDateString(); // Ajuste para formatar como DD/MM/YYYY
     ret.id = ret._id.toString();
     delete ret._id;
     delete ret.__v;
