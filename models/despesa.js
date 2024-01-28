b4e52527d55d269f806261d0f467f6cd2d774b4b
Eduardo Lange <eduardoandrelange@gmail.com>
2024-01-28 20:20:30 -0300

    refactoring

--- models/despesa.js
+++ models/despesa.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const despesaSchema = new mongoose.Schema({
-  dia: { type: String, required: true },
-  valor: { type: Number, required: true },
-  observacao: { type: String },
-});
+	dia: { type: String, required: true },
+	valor: { type: Number, required: true },
+	observacao: { type: String },
+})
 
 // Definir o formato desejado ao serializar para JSON
 despesaSchema.set('toJSON', {
-  transform: function (doc, ret) {
-    // tirei essa merda pq eu tive que enviar uma string invés de data.
-    // ret.dia = ret.dia.toLocaleDateString(); // Ajuste para formatar como DD/MM/YYYY
-    ret.id = ret._id.toString();
-    delete ret._id;
-    delete ret.__v;
-  },
-});
+	transform: function (doc, ret) {
+		// tirei essa merda pq eu tive que enviar uma string invés de data.
+		// ret.dia = ret.dia.toLocaleDateString(); // Ajuste para formatar como DD/MM/YYYY
+		ret.id = ret._id.toString()
+		delete ret._id
+		delete ret.__v
+	},
+})
 
-module.exports = mongoose.model('Despesa', despesaSchema);
+module.exports = mongoose.model('Despesa', despesaSchema)
