5a929f99b018ce07109c4e1e94cd2b2d3c4d8c2c
Eduardo Lange <eduardoandrelange@gmail.com>
2024-02-23 20:53:40 -0300

    logger não fucnionando adequadamente

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,13 +1,12 @@
 const logger = require('./logger')
 
 const requestLogger = (request, response, next) => {
-  logger.info('Method:', request.method);
-  logger.info('Path:  ', request.path);
-  logger.info('Body:  ', request.body);
-  logger.info('---');
-  console.log('Request object:', request.method);
-  next();
-};
+	console.log('Method:', request.method)
+	console.log('Path:  ', request.path)
+	console.log('Body:  ', request.body)
+	console.log('---')
+	next()
+}
 
 const unknownEndpoint = (request, response) => {
 	response.status(404).send({ error: 'endpoint desconhecido' })
