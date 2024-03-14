af7ce736514104ee8cea26fabc6c8787dc8d21e3
Eduardo Lange <eduardoandrelange@gmail.com>
2024-03-13 22:01:38 -0300

    Update despesas.js

--- routes/despesas.js
+++ routes/despesas.js
@@ -1,56 +1,54 @@
-const express = require('express');
-const Despesa = require('../models/despesa');
+const express = require('express')
+const Despesa = require('../models/despesa')
 
-const router = express.Router();
+const router = express.Router()
 
 // Rota para obter todas as despesas
 router.get('/', async (request, response) => {
-  try {
-    const despesas = await Despesa.find({})
-    response.setHeader('Content-Type', 'application/json');
-    response.json(despesas);
-  } catch (error) {
-    console.error('Erro na consulta ao MongoDB:', error);
-    response.status(500).json({ error: 'Erro interno do servidor' });
-  }
-});
+	try {
+		const despesas = await Despesa.find({})
+		response.setHeader('Content-Type', 'application/json')
+		response.json(despesas)
+	} catch (error) {
+		console.error('Erro na consulta ao MongoDB:', error)
+		response.status(500).json({ error: 'Erro interno do servidor' })
+	}
+})
 
 // Rota para criar uma nova despesa
 router.post('/', async (request, response) => {
+	try {
+		console.log(request.body)
+		const novaDespesa = new Despesa({
+			dia: request.body.dia,
+			valor: request.body.valor,
+			observacao: request.body.observacao,
+			categoria: request.body.categoria,
+		})
 
-  try {
-    console.log(request.body)
-    const novaDespesa = new Despesa({
-      dia: request.body.dia,
-      valor: request.body.valor,
-      observacao: request.body.observacao,
-      categoria: request.body.categoria,
-    });
+		// Salvar a nova despesa no banco de dados
+		await novaDespesa.save()
 
-    // Salvar a nova despesa no banco de dados
-    await novaDespesa.save();
-
-    // Responder com uma mensagem de sucesso ou a despesa criada
-    response.status(201).json({ message: 'Despesa criada com sucesso', data: novaDespesa });
-  } catch (error) {
-    console.error('Erro ao criar despesa:', error);
-    response.status(500).json({ error: 'Erro interno do servidor' });
-  }
-});
+		// Responder com uma mensagem de sucesso ou a despesa criada
+		response.status(201).json({ message: 'Despesa criada com sucesso', data: novaDespesa })
+	} catch (error) {
+		console.error('Erro ao criar despesa:', error)
+		response.status(500).json({ error: 'Erro interno do servidor' })
+	}
+})
 
 router.delete('/:despesaId', async (req, res) => {
-  try {
-    const despesaId = req.params.despesaId;
+	try {
+		const despesaId = req.params.despesaId
 
-    // Execute a lógica para excluir a despesa no banco de dados
-    await Despesa.findByIdAndDelete(despesaId);
+		// Execute a lógica para excluir a despesa no banco de dados
+		await Despesa.findByIdAndDelete(despesaId)
 
-    res.status(200).json({ message: 'Despesa excluída com sucesso' });
-  } catch (error) {
-    console.error('Erro ao excluir despesa:', error);
-    res.status(500).json({ error: 'Erro ao excluir despesa' });
-  }
-});
+		res.status(200).json({ message: 'Despesa excluída com sucesso' })
+	} catch (error) {
+		console.error('Erro ao excluir despesa:', error)
+		res.status(500).json({ error: 'Erro ao excluir despesa' })
+	}
+})
 
-
-module.exports = router;
+module.exports = router
